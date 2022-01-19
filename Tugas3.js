88e8daa1464d116be4c8fb2e6a9429d15d8c2117
HirasParasian <hirasparasian@gmail.com>
2022-01-19 08:09:13 +0700

    JS 2

--- Tugas3.js
+++ Tugas3.js
@@ -1,18 +1,13 @@
 const nilai = [2, 25, 4, 14, 17, 30, 8]
 
 const SeleksiNilai = (num1, num2, dataArray) => {
-    if (typeof num1 !== "number" || typeof num2 !== "number" || !Array.isArray(dataArray)) { 
-        return 0;
-    }
-
-    if (num1 > num2) { 
-        console.log("Nilai akhir harus lebih besar dari nilai awal");
-        return 0;
-    }
-
-    if (dataArray.length <= 5) { 
-        console.log("Jumlah angka dalam dataArray harus lebih dari 5");
-        return 0;
+    if (typeof num1 == "number" || typeof num2 == "number" || !Array.isArray(dataArray)) { 
+      if (num1 > num2) { 
+          console.log("Nilai akhir harus lebih besar dari nilai awal");
+        if (dataArray.length <= 5) { 
+            console.log("Jumlah angka dalam dataArray harus lebih dari 5");       
+        }     
+      }     
     }
     const hasil = dataArray.filter(el => el >= num1 && el <= num2)  
 
