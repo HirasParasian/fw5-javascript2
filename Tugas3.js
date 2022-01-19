2605a1d077f7c56b05ea3f46dd125d1dad73f5db
HirasParasian <hirasparasian@gmail.com>
2022-01-19 14:35:04 +0700

    File JS2

--- Tugas3.js
+++ Tugas3.js
@@ -1,16 +1,26 @@
-const nilai = [2, 25, 4]
+const values = [2, 25, 4, 14, 17, 30, 8]
 
-const SeleksiNilai = (num1, num2, dataArray) => {
-    if (typeof num1 == "number" || typeof num2 == "number" || !Array.isArray(dataArray)) { 
-      if (num1 > num2) { 
-          console.log("Nilai akhir harus lebih besar dari nilai awal");
-        if (dataArray.length <= 5) { 
-            console.log("Jumlah angka dalam dataArray harus lebih dari 5");       
-        }     
-      }     
-    }
-    const hasil = dataArray.filter(fil => fil >= num1 && fil <= num2)  
-    const akhir = hasil.sort((a, b) => a - b) 
-    console.log(hasil)
+function seleksiNilai (limit1, limit2, data) {
+
+  if (typeof limit1 !== "number" || typeof limit2 !== 'number' || !Array.isArray(data)) {
+    return 'Data tidak valid'
+  }
+  if (limit1 > limit2) {
+    return 'Nilai akhir harus lebih besar dari nilai awal'
+  }
+  if (data.length <= 5) {
+    return 'Jumlah angka dalam dataArray harus lebih dari 5'
+  }
+
+  const result = data.filter(function(el){
+      return el >= limit1 && el <= limit2
+  })
+
+  const final = result.sort(function(a,b){
+    return a-b
+    })
+
+  console.log(final);
 }
-SeleksiNilai(16,15, nilai)
+
+seleksiNilai(20, 100, values)
